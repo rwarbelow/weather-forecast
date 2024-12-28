500bdfeb6f9150cf035f2ae274353f99f25776a3
Rachel Warbelow <rachelwarbelow@gmail.com>
2024-12-27 22:57:57 -0600

    Adds weather api service and spec

---
+++ app/services/weather_api_service.rb
@@ -0,0 +1,54 @@
+require 'net/http'
+
+class WeatherApiService
+  BASE_URL = "http://api.weatherapi.com/v1"
+  CACHE_EXPIRATION = 30.minutes
+
+  def initialize(zip_code)
+    @zip_code = zip_code
+  end
+
+  def weather_result
+    data = fetch_weather_data
+    WeatherResult.new(
+      cache_hit: data[:cache_hit],
+      zip_code: @zip_code,
+      current_temperature: data[:current_temperature],
+      forecasts: data[:forecasts]
+    )
+  end
+
+  private
+  
+  def fetch_weather_data
+    cache_hit = true
+    cache_key = "weather_data_#{@zip_code}"
+    weather_result = Rails.cache.fetch(cache_key, expires_in: CACHE_EXPIRATION) do
+      cache_hit = false
+      { current_temperature: get_current_temperature, forecasts: get_forecasts }
+    end
+    weather_result.merge(cache_hit: cache_hit)
+  end
+
+  def get_current_temperature
+    make_request(api_name: "current").dig("current", "temp_f")
+  end
+
+  def get_forecasts(days: 4)
+    forecast_data = make_request(api_name: "forecast", query_params: { days: days }).dig("forecast", "forecastday")
+    make_daily_forecasts(forecast_data)
+  end
+
+  def make_daily_forecasts(forecast_data)
+    forecast_data.map do |day|
+      DailyForecast.new(date: day["date"], high: day["day"]["maxtemp_f"], low: day["day"]["mintemp_f"])
+    end
+  end
+
+  def make_request(api_name:, query_params: {})
+    query_params.merge!(q: @zip_code)
+    url = URI("#{BASE_URL}/#{api_name}.json?key=#{ENV['WEATHER_API_KEY']}&#{query_params.to_query}")
+    response = Net::HTTP.get(url)
+    JSON.parse(response)
+  end
+end
---
+++ spec/services/weather_api_service_spec.rb
@@ -0,0 +1,54 @@
+require "rails_helper"
+
+RSpec.describe WeatherApiService do
+  let(:today) { Date.today.to_s }
+  let(:zip_code) { rand(10000..99999) }
+  let(:maxtemp_f) { rand(0..120) }
+  let(:mintemp_f) { rand(0..120) }
+  let(:expected_current_temp) { rand(mintemp_f..maxtemp_f) }
+  let(:raw_forecast_data) { [{ "date": today, "day": { "maxtemp_f": maxtemp_f, "mintemp_f": mintemp_f}}]}
+  let(:cache_key) { "weather_data_#{zip_code}"}
+  let(:forecast_url) { URI("#{WeatherApiService::BASE_URL}/forecast.json?key=#{ENV['WEATHER_API_KEY']}&days=5&q=#{zip_code}") }
+  let(:current_temp_url) { URI("#{WeatherApiService::BASE_URL}/current.json?key=#{ENV['WEATHER_API_KEY']}&q=#{zip_code}") }
+  let(:memory_store) { ActiveSupport::Cache.lookup_store(:memory_store) }
+  
+  before do
+    allow(Net::HTTP).to receive(:get).with(current_temp_url).and_return(JSON.generate({ current: { temp_f: expected_current_temp }}))
+    allow(Net::HTTP).to receive(:get).with(forecast_url).and_return(JSON.generate({ forecast: { forecastday: raw_forecast_data }}))
+    allow(Rails).to receive(:cache).and_return(memory_store)
+  end
+
+  describe "#weather_result" do
+    context "when result is not found in cache" do
+      it "returns a WeatherResult object with the correct attributes" do
+        allow(Rails.cache).to receive(:fetch)
+          .with(cache_key, expires_in: WeatherApiService::CACHE_EXPIRATION).and_yield()
+
+        service = described_class.new(zip_code)
+        result = service.weather_result
+
+        expect(result).to be_a(WeatherResult)
+        expect(result.cache_hit).to be(false)
+        expect(result.current_temperature).to eq(expected_current_temp)
+        expect(result.forecasts.count).to eq(raw_forecast_data.count)
+        expect(result.forecasts).to all(be_a(DailyForecast))
+      end
+    end
+
+    context "when result is found in cache" do
+      it "returns a WeatherResult object with the correct attributes" do
+        allow(Rails.cache).to receive(:fetch)
+          .with(cache_key, expires_in: WeatherApiService::CACHE_EXPIRATION)
+          .and_return({ current_temperature: expected_current_temp, forecasts: [] })
+
+        service = described_class.new(zip_code)
+        result = service.weather_result
+
+        expect(result).to be_a(WeatherResult)
+        expect(result.cache_hit).to be(true)
+        expect(result.current_temperature).to eq(expected_current_temp)
+        expect(result.forecasts).to be_empty
+      end
+    end
+  end
+end
