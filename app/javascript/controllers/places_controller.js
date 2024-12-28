8de9427ae536d317c87dc19c414eaf0775574589
Rachel Warbelow <rachelwarbelow@gmail.com>
2024-12-28 11:34:22 -0600

    Modifies js controller

--- app/javascript/controllers/places_controller.js
+++ app/javascript/controllers/places_controller.js
@@ -1,27 +1,36 @@
-import { Controller } from "stimulus"
+import { Controller } from "stimulus";
 
 export default class extends Controller {
-  static targets = [ "field" ]
+  static targets = ["field"];
 
   connect() {
-    window.addEventListener('google-maps-callback', () => {
+    window.addEventListener("google-maps-callback", () => {
       this.initializeAutocomplete();
     });
+
+    this.element.addEventListener("keydown", (event) => this.preventFormSubmission(event));
   }
 
-  // add Google Maps Autocomplete and listen for place_changed event
+  // Add Google Maps Autocomplete and listen for place_changed event
   initializeAutocomplete() {
     this.autocomplete = new google.maps.places.Autocomplete(this.fieldTarget);
-    this.autocomplete.addListener('place_changed', () => {
-      this.setCoordinates();
+    this.autocomplete.addListener("place_changed", () => {
+      this.setZipCode();
     });
   }
 
-  // when place_changed, get zip code from place and set as zip_code input
-  setCoordinates() {
+  // When place_changed, get zip code from place and set as zip_code input
+  setZipCode() {
     const place = this.autocomplete.getPlace();
-    const zipCode = place.address_components.find((c) => c.types.includes('postal_code'));
+    const zipCode = place.address_components.find((c) => c.types.includes("postal_code"));
     const zipCodeField = this.element.querySelector('input[name="zip_code"]');
-    zipCodeField.value = zipCode.long_name;
+    zipCodeField.value = zipCode?.long_name || "";
+  }
+
+  // Don't submit form when enter is pressed; only select option from dropdown
+  preventFormSubmission(event) {
+    if (event.key === "Enter" && event.target === this.fieldTarget) {
+      event.preventDefault();
+    }
   }
 }
